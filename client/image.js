e75999e7d60d154e882e2262fbe23426cb4c7d3d
Daniel Flynn <flynndcs@gmail.com>
2020-05-31 11:47:09 -0500

    satellite image page implemented for 3 regions

--- client/image.js
+++ client/image.js
@@ -1,15 +1,24 @@
 var imgDiv = document.getElementById("img");
-console.log(imgDiv);
 
 var imgDate = document.getElementById("imgSelect");
-console.log(imgDate.value);
+
+var citySelector = document.getElementById("citySelect");
 
 var selectedDate;
+var selectedCity;
+
+citySelector.addEventListener('change', (event) => {
+    console.log(event.target.value);
+    selectedCity = event.target.value;
+});
+
 imgDate.addEventListener('change', (event) => {
     console.log(event.target.value);
     selectedDate = event.target.value;
-    var fetchURL = "https://elasticbeanstalk-us-east-1-135815009727.s3.amazonaws.com/Sharing/Texas+Images/" + selectedDate + ".png";
+    var fetchURL = "https://elasticbeanstalk-us-east-1-135815009727.s3.amazonaws.com/Sharing/" + selectedCity + "Images/" + selectedDate + ".png";
     console.log(fetchURL);
     imgDiv.setAttribute('src', fetchURL)
 });
+
+
 
